360ad837aaa446e82daa2952178af30fa88bd591
venicius-braco <gourlarte.e.fod4@gmail.com>
2024-11-25 08:43:21 -0300

    Alteração dos dados do cliente pelo perfil de administrador

--- Carstec/administradorClientesAlterar.cs
+++ Carstec/administradorClientesAlterar.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Carstec
 {
@@ -17,6 +18,39 @@ namespace Carstec
         {
             InitializeComponent();
             id = i;
+            MySqlConnection conectar = new MySqlConnection("SERVER=localhost;DATABASE=carstec;UID=root;PASSWORD=");
+            conectar.Open();
+            MySqlCommand consulta = new MySqlCommand();
+            consulta.Connection = conectar;
+            consulta.CommandText = "SELECT * FROM cliente WHERE cliente.id = "+ id;
+            MySqlDataReader resultado = consulta.ExecuteReader();
+            if (resultado.HasRows)
+            {
+                while (resultado.Read())
+                {
+                    textBox1.Text = resultado["nome"].ToString();
+                    textBox2.Text = resultado["email"].ToString();
+                    textBox3.Text = resultado["cpf"].ToString();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nenhum registro foi encontrado!");
+            }
+            conectar.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conexao = new MySqlConnection();
+            conexao.ConnectionString = ("SERVER=localhost;DATABASE=carstec;UID=root;PASSWORD=");
+            conexao.Open();
+            string inserir = "UPDATE cliente SET nome = '" + textBox1.Text + "', email = '" + textBox2.Text + "', cpf = '" + textBox3.Text + "' WHERE cliente.id = " + id;
+            MySqlCommand comandos = new MySqlCommand(inserir, conexao);
+            comandos.ExecuteNonQuery();
+            conexao.Close();
+            MessageBox.Show("Cliente atualizado com sucesso!");
+            this.Close();
         }
     }
 }
