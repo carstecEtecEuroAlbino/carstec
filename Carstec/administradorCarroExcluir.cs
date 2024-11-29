3257ec72a7bd2c01b7c8587bff67dfdb7d1c5a14
venicius-braco <gourlarte.e.fod4@gmail.com>
2024-11-29 08:33:44 -0300

    Exclusão dos carros no módulo do administrador.

--- Carstec/administradorCarroExcluir.cs
+++ Carstec/administradorCarroExcluir.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using MySql.Data.MySqlClient;
 namespace Carstec
 {
     public partial class administradorCarroExcluir : Form
@@ -17,6 +17,37 @@ namespace Carstec
         {
             InitializeComponent();
             id = i;
+            MySqlConnection conectar = new MySqlConnection("SERVER=localhost;DATABASE=carstec;UID=root;PASSWORD=");
+            conectar.Open();
+            MySqlCommand consulta = new MySqlCommand();
+            consulta.Connection = conectar;
+            consulta.CommandText = "SELECT * FROM carro WHERE carro.id = " + id;
+            MySqlDataReader resultado = consulta.ExecuteReader();
+            if (resultado.HasRows)
+            {
+                while (resultado.Read())
+                {
+                    textBox1.Text = resultado["id"].ToString();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nenhum registro foi encontrado!");
+            }
+            conectar.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conexao = new MySqlConnection();
+            conexao.ConnectionString = ("SERVER=127.0.0.1; DATABASE=carstec; UID = root; PASSWORD = ; ");
+            conexao.Open();
+            string inserir = "DELETE FROM carro WHERE carro.id = " + id;
+            MySqlCommand comandos = new MySqlCommand(inserir, conexao);
+            comandos.ExecuteNonQuery();
+            conexao.Close();
+            MessageBox.Show("Carro excluido com sucesso!");
+            this.Close();
         }
     }
 }
