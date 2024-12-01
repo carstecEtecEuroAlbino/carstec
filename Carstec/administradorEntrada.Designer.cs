b02cbde67e8699f78b8580cee33b85d69fa3a522
Giovani D. Santos <maracaruja@gmail.com>
2024-12-01 11:23:23 -0300

    ADM cadastro em nova pagina, arrumacao de poblemas

--- Carstec/administradorEntrada.Designer.cs
+++ Carstec/administradorEntrada.Designer.cs
@@ -35,19 +35,11 @@ namespace Carstec
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label7 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(534, 228);
+            this.button2.Location = new System.Drawing.Point(334, 231);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 21;
@@ -58,7 +50,7 @@ namespace Carstec
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(518, 186);
+            this.label6.Location = new System.Drawing.Point(318, 189);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(36, 13);
             this.label6.TabIndex = 20;
@@ -67,7 +59,7 @@ namespace Carstec
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(518, 125);
+            this.label5.Location = new System.Drawing.Point(318, 128);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(31, 13);
             this.label5.TabIndex = 19;
@@ -75,14 +67,14 @@ namespace Carstec
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(521, 202);
+            this.textBox3.Location = new System.Drawing.Point(321, 205);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 18;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(521, 141);
+            this.textBox4.Location = new System.Drawing.Point(321, 144);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(100, 20);
             this.textBox4.TabIndex = 17;
@@ -91,93 +83,17 @@ namespace Carstec
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(516, 69);
+            this.label2.Location = new System.Drawing.Point(316, 72);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(53, 25);
             this.label2.TabIndex = 16;
             this.label2.Text = "login";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(183, 254);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 40;
-            this.button1.Text = "cadastrar";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(168, 212);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(36, 13);
-            this.label7.TabIndex = 39;
-            this.label7.Text = "senha";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(171, 228);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 37;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(168, 170);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(31, 13);
-            this.label4.TabIndex = 35;
-            this.label4.Text = "email";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(168, 125);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(33, 13);
-            this.label3.TabIndex = 34;
-            this.label3.Text = "nome";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(166, 69);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 25);
-            this.label1.TabIndex = 33;
-            this.label1.Text = "Cadastro";
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(171, 186);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 20);
-            this.textBox5.TabIndex = 32;
-            // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(171, 141);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 20);
-            this.textBox6.TabIndex = 31;
             // 
             // administradorEntrada
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox6);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -186,6 +102,7 @@ namespace Carstec
             this.Controls.Add(this.label2);
             this.Name = "administradorEntrada";
             this.Text = "administradorEntrada";
+            this.Load += new System.EventHandler(this.administradorEntrada_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -199,13 +116,5 @@ namespace Carstec
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
     }
 }
--- Carstec/administradorEntrada.cs
+++ Carstec/administradorEntrada.cs
@@ -83,59 +83,12 @@ namespace Carstec
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string connectionString = "SERVER=localhost;DATABASE=carstec;UID=root;PASSWORD=;";
+           
+        }
 
-            // Capturar os dados do formulário
-            string nome = textBox6.Text;  // Nome do administrador
-            string email = textBox5.Text; // Email do administrador
-            string senha = textBox1.Text; // Senha do administrador
+        private void administradorEntrada_Load(object sender, EventArgs e)
+        {
 
-            // Validação básica dos campos
-            if (string.IsNullOrWhiteSpace(nome) || string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(senha))
-            {
-                MessageBox.Show("Por favor, preencha todos os campos.");
-                return;
-            }
-
-            try
-            {
-                using (MySqlConnection conexao = new MySqlConnection(connectionString))
-                {
-                    conexao.Open();
-
-                    // Verificar se o email já existe no banco de dados
-                    string checkQuery = "SELECT COUNT(*) FROM administrador WHERE email = @Email";
-                    using (MySqlCommand checkCommand = new MySqlCommand(checkQuery, conexao))
-                    {
-                        checkCommand.Parameters.AddWithValue("@Email", email);
-                        int count = Convert.ToInt32(checkCommand.ExecuteScalar());
-
-                        if (count > 0)
-                        {
-                            MessageBox.Show("Erro: Já existe um administrador com este email.");
-                            return;
-                        }
-                    }
-
-                    // Inserir o novo administrador
-                    string inserir = "INSERT INTO administrador (nome, email, senha) VALUES (@Nome, @Email, @Senha)";
-                    using (MySqlCommand comandos = new MySqlCommand(inserir, conexao))
-                    {
-                        comandos.Parameters.AddWithValue("@Nome", nome);
-                        comandos.Parameters.AddWithValue("@Email", email);
-                        comandos.Parameters.AddWithValue("@Senha", senha); 
-
-                        comandos.ExecuteNonQuery();
-                        MessageBox.Show("Administrador cadastrado com sucesso!");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Erro ao cadastrar administrador: {ex.Message}");
-            }
-            administradorHome cadCliente = new administradorHome();
-            cadCliente.Show();
         }
     }
     
